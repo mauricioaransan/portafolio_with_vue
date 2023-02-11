3b8dbb5ac930b5baff35c9f45c8877d1b4ac7c89
angel <angel.floreano@deister.es>
2023-02-11 00:45:43 -0500

    created new EditedButton / add css in aboutme

---
+++ src/components/Buttons/BotonHover.vue
@@ -0,0 +1,157 @@
+<template>
+  <a :style="colors">
+    {{ title }}
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+
+  </a>
+</template>
+
+<script lang="ts">
+import {computed, defineComponent, ref} from "vue";
+
+export default defineComponent({
+  name: "BotonHover",
+  props:{
+    colorHover:{
+      type:String,
+      required:true
+    },
+    title:{
+      type:String,
+      required:true
+    }
+  },
+
+  setup(props){
+    const colors = computed(() => {
+      return {
+        '--color' : props.colorHover
+      }
+    })
+
+    return{
+      colors
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+a{
+  position: relative;
+  display: inline-block;
+  color: rgba(255,255,255,.4) !important;
+  font-size: 20px;
+  letter-spacing: 5px;
+  text-decoration: none;
+  text-transform: uppercase;
+  background-color: #262c37;
+  padding: 10px 30px;
+  transition: .5s;
+  transition-delay: .8s;
+}
+a:hover{
+  color: var(--color);
+  text-shadow: 0 0 5px var(--color);
+  letter-spacing: .25em;
+  transition-delay: 1ms;
+}
+a span{
+  position: absolute;
+  display: block;
+  background: var(--color);
+  text-shadow: 0 0 5px var(--color);
+}
+a span:nth-child(1){
+  left: 0;
+  top: 0;
+  width: 50.5%;
+  height: 2px;
+  transform-origin: left;
+  transform: scaleX(0);
+  transition: transform .5s;
+}
+a:hover span:nth-child(1){
+  transform: scaleX(1);
+  transform-origin: right;
+}
+
+a span:nth-child(2){
+  right: 0;
+  top: 0;
+  width: 50.5%;
+  height: 2px;
+  transform-origin: right;
+  transform: scaleX(0);
+  transition: transform .5s;
+}
+a:hover span:nth-child(2){
+  transform: scaleX(1);
+  transform-origin: left;
+}
+
+a span:nth-child(3){
+  right: 0;
+  top: 0;
+  width: 2px;
+  height: 100%;
+  transform-origin: bottom;
+  transform: scaleY(0);
+  transition: transform .5s;
+  transition-delay: .4s;
+}
+a:hover span:nth-child(3){
+  transform: scaleY(1);
+  transform-origin: top;
+}
+
+a span:nth-child(4){
+  left: 0;
+  top: 0;
+  width: 2px;
+  height: 100%;
+  transform: scaleY(0);
+  transform-origin: bottom;
+  transition: transform .5s;
+  transition-delay: .4s;
+}
+a:hover span:nth-child(4){
+  transform: scaleY(1);
+  transform-origin: top;
+}
+
+a span:nth-child(5){
+  left: 0;
+  bottom: 0;
+  width: 50.5%;
+  height: 2px;
+  transform-origin: right;
+  transform: scaleX(0);
+  transition: transform .5s;
+  transition-delay: .8s;
+}
+a:hover span:nth-child(5){
+  transform: scaleX(1);
+  transform-origin: left;
+}
+a span:nth-child(6){
+  right: 0;
+  bottom: 0;
+  width: 50.5%;
+  height: 2px;
+  transform-origin: left;
+  transform: scaleX(0);
+  transition: transform .5s;
+  transition-delay: .8s;
+}
+a:hover span:nth-child(6){
+  transform: scaleX(1);
+  transform-origin: right;
+}
+</style>
--- src/components/about/aboutme.vue
+++ src/components/about/aboutme.vue
@@ -1,5 +1,5 @@
 <template>
-<v-sheet>
+<v-sheet >
   <v-card
       height="100vh"
       color="#008000"
@@ -29,39 +29,50 @@
 
 
   </v-card>
-  <v-sheet id="about">
-    <h1>Acerca de mí</h1>
+  <v-sheet id="about" class="mt-10 pl-5 pr-5" color="black">
+
+
+    <h1 class="port_title">Acerca de mí</h1>
     <div class="under_line_title"></div>
     <v-row >
-      <v-col md="5" lg="6" xl="6">
-        Tale adolescens faucibus pertinax vero. Quis nisl iriure putent animal.
-        Consequat iudicabit senserit agam noluisse montes facilisi dictum hendrerit sit.
-        Molestiae constituto pertinax nunc vituperata fringilla explicari persius.
-        Ponderum convenire bibendum autem facilis mediocritatem vocibus. Eos mus nullam postulant mea nisi dicam euripidis aptent.
-        Augue recteque risus wisi mediocrem prodesset.
+      <v-col md="5" lg="6" xl="6" class="about_text_principal">
+        <v-sheet width="85%" style="margin: 0 auto" elevation="5" class="pa-5">
+          <span class="about_text_develop">Desarrollador FrontEnd </span>
+          <span> con conocimientos en Vue, JS, HTML y CSS,
+        además de otros lenguajes como TypeScript y Java.
+        Egresado de la carrera de Ingeniería de Sistemas con muchas facilidades de aprendizaje rápido
+        y efectivo, con ganas de seguir adquiriendo conocimientos.
+        Me considero una persona con una gran prioridad por la responsabilidad y
+        el compromiso por el trabajo, además de tener aptitudes para
+        poder desarrollar bajo presión.
+        </span>
+        </v-sheet>
       </v-col>
 
       <v-col md="7" lg="6" xl="6" >
-        <h4>Personal Info</h4>
-        <v-row>
-          <v-col cols="6">
-           <div> <span class="personal_info_header"> Nombre </span> <span class="personal_info_info"> Mauricio Arancibia</span> </div>
-           <div> <span class="personal_info_header"> Edad </span> <span class="personal_info_info"> 26 años</span> </div>
-           <div> <span class="personal_info_header"> Email </span> <span class="personal_info_info"> mauricioaransan@gmail.com</span> </div>
-           <div> <span class="personal_info_header"> Dirección </span> <span class="personal_info_info"> Villa el Salvador - Lima </span> </div>
-          </v-col>
-          <v-col cols="6">
-            <div> <span class="personal_info_header"> Habilidad </span> <span class="personal_info_info"> Front-End</span> </div>
-            <div> <span class="personal_info_header"> Experiencia </span> <span class="personal_info_info"> 1 año</span> </div>
-            <div> <span class="personal_info_header"> Lenguajes </span> <span class="personal_info_info"> Español / Ingles (escrito)</span> </div>
-            <div> <span class="personal_info_header"> FreeLance </span> <span class="personal_info_info"> Disponible </span> </div>
-          </v-col>
-        </v-row>
-        <v-row class="ma-0 mt-5">
-          <v-btn color="red">
-            Descarga CV
-          </v-btn>
-        </v-row>
+
+          <h3 class="port_subtitle">Personal Info</h3>
+          <v-row>
+            <v-col cols="6">
+              <v-sheet elevation="5" class="pa-5 personal_info_card" style="" >
+                <div> <span class="personal_info_header"> Nombre: </span> <span class="personal_info_info"> Mauricio Arancibia</span> </div>
+                <div> <span class="personal_info_header"> Edad: </span> <span class="personal_info_info"> 26 años</span> </div>
+                <div> <span class="personal_info_header"> Email: </span> <span class="personal_info_info"> mauricioaransan@gmail.com</span> </div>
+                <div> <span class="personal_info_header"> Dirección: </span> <span class="personal_info_info"> Villa el Salvador - Lima </span> </div>
+              </v-sheet>
+            </v-col>
+            <v-col cols="6">
+              <v-sheet elevation="5" class="pa-5 personal_info_card">
+                <div> <span class="personal_info_header"> Habilidad: </span> <span class="personal_info_info"> Front-End</span> </div>
+                <div> <span class="personal_info_header"> Experiencia: </span> <span class="personal_info_info"> 1 año</span> </div>
+                <div> <span class="personal_info_header"> Lenguajes: </span> <span class="personal_info_info"> Español / Ingles (escrito)</span> </div>
+                <div> <span class="personal_info_header"> FreeLance: </span> <span class="personal_info_info"> Disponible </span> </div>
+              </v-sheet>
+            </v-col>
+          </v-row>
+          <v-row class="ma-0 mt-5">
+            <botonHover colorHover="red" title="Descargar cv"/>
+          </v-row>
       </v-col>
     </v-row>
 
@@ -143,6 +154,7 @@ import Skills from "@/components/skill/skills.vue";
 import ContactMe from "@/components/contactme/contactme.vue";
 import Portafolio from "@/components/Portafolio/Portafolio.vue";
 import Typewriter from "@/components/tipper.vue"
+import botonHover from "@/components/Buttons/BotonHover.vue"
 
 export default defineComponent({
   name: "aboutme",
@@ -153,10 +165,12 @@ export default defineComponent({
     TimeLineExperience,
     Skills,
     ContactMe,
-    Typewriter
+    Typewriter,
+    botonHover,
   },
 
   setup(){
+    const colorFromScript = ref('#000')
   const model = ref(0);
   const items = ref([
     {title: 'Experiencia'},
@@ -185,7 +199,7 @@ export default defineComponent({
     },4000)
   })
     return{
-    model,items,text_typed,time,replace
+    model,items,text_typed,time,replace,colorFromScript
     }
   },
 
@@ -206,6 +220,34 @@ export default defineComponent({
 </script>
 
 <style scoped>
+
+.port_title{
+  text-transform: uppercase;
+  color:  white;
+}
+.port_subtitle{
+  text-transform: uppercase;
+  color: red;
+  margin-bottom: 5px;
+}
+
+.about_text_principal{
+  text-align: justify;
+}
+.about_text_develop{
+  font-weight: 600;
+  text-transform: uppercase;
+  font-size: 20px;
+
+}
+.personal_info_card{
+  border-radius: 20px;
+}
+.personal_info_header{
+  font-weight: bold;
+  margin-right: 10px;
+}
+
 .under_line_title{
   width: 100px;
   height: 10px;
@@ -213,7 +255,6 @@ export default defineComponent({
   padding-top: 15px;
   padding-bottom: 15px;
 }
-
 
 .experience_card{
   transition: 2s;
